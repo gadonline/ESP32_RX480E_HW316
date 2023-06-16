8ed641b6c6b49ac464ebc6baa6b20a33f2c9f268
Artem Majorov <gad37rus@gmail.com>
2023-06-17 00:36:50 +0300

    add support radio switch

--- main/gcov_example_main.c
+++ main/gcov_example_main.c
@@ -1,11 +1,3 @@
-/* Blink Example with covergae info
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -13,64 +5,53 @@
 #include "esp_app_trace.h"
 #include "sdkconfig.h"
 
-/* Can use project configuration menu (idf.py menuconfig) to choose the GPIO
-   to blink, or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO 25
+struct relay_struct {
+    int gpio_output_number;
+    int gpio_output_level;
+    int gpio_input_number;
+    int gpio_input_last;
+};
+struct relay_struct relay_list[4];
 
-void blink_dummy_func(void);
-void some_dummy_func(void);
-
-static void blink_task(void *pvParameter)
-{
-    /*
-    gpio_reset_pin(16);
-    gpio_reset_pin(17);
-    gpio_reset_pin(25);
-    gpio_reset_pin(26);
-    */
-    gpio_set_direction(16, GPIO_MODE_OUTPUT);
-    gpio_set_direction(17, GPIO_MODE_OUTPUT);
-    gpio_set_direction(25, GPIO_MODE_OUTPUT);
-    gpio_set_direction(26, GPIO_MODE_OUTPUT);
-    gpio_set_level(16, 1);
-    gpio_set_level(17, 1);
-    gpio_set_level(25, 1);
-    gpio_set_level(26, 1);
-    while(1) {
-        gpio_set_level(16, 0);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-        gpio_set_level(16, 1);
-        gpio_set_level(17, 0);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-        gpio_set_level(17, 1);
-        gpio_set_level(25, 0);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-        gpio_set_level(25, 1);
-        gpio_set_level(26, 0);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-        gpio_set_level(26, 1);
-        //gpio_set_level(25, 1);
-        //vTaskDelay(2000 / portTICK_PERIOD_MS);
-        //gpio_set_level(26, 0);
-        //vTaskDelay(2000 / portTICK_PERIOD_MS);
-
-        //gpio_set_level(26, 1);
-        //vTaskDelay(2000 / portTICK_PERIOD_MS);
-        /*
-        blink_dummy_func();
-        some_dummy_func();
-        if (dump_gcov_after++ < 0) {
-            // Dump gcov data
-            printf("Ready to dump GCOV data...\n");
-            esp_gcov_dump();
-            printf("GCOV data have been dumped.\n");
-        }
-        */
-    }
-}
+int relay_count = 4;
 
 void app_main(void)
 {
-    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+    relay_list[0].gpio_output_number = 16;
+    relay_list[0].gpio_input_number  = 34;
+    relay_list[1].gpio_output_number = 17;
+    relay_list[1].gpio_input_number  = 35;
+    relay_list[2].gpio_output_number = 25;
+    relay_list[2].gpio_input_number  = 36;
+    relay_list[3].gpio_output_number = 26;
+    relay_list[3].gpio_input_number  = 39;
+    
+    int i;
+    int input_level;
+    for (i = 0; i<relay_count; i++)
+    {
+        gpio_set_direction(relay_list[i].gpio_output_number, GPIO_MODE_OUTPUT);
+        gpio_set_direction(relay_list[i].gpio_input_number, GPIO_MODE_INPUT);
+        gpio_set_level(relay_list[i].gpio_output_number, 1);
+        relay_list[i].gpio_output_level = 1;
+        relay_list[i].gpio_input_last = gpio_get_level(relay_list[i].gpio_input_number);
+    }
+    
+    while(1) {
+        for (i = 0; i<relay_count; i++)
+        {
+            input_level = gpio_get_level(relay_list[i].gpio_input_number);
+            if (relay_list[i].gpio_input_last != input_level) {
+                if (relay_list[i].gpio_output_level == 0) {
+                    relay_list[i].gpio_output_level = 1;
+                } else {
+                    relay_list[i].gpio_output_level = 0;
+                }
+                printf("relay %d set level: %d\n", i, relay_list[i].gpio_output_level);
+                gpio_set_level(relay_list[i].gpio_output_number, relay_list[i].gpio_output_level);
+                relay_list[i].gpio_input_last = input_level;
+            }
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
 }
